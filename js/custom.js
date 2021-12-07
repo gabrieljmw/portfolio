8efcd3ab56822342bcd528d58add135e0cff7a3c
Gabriel Jahbez <45593692+gabrieljmw@users.noreply.github.com>
2021-12-06 21:46:58 -0500

    Tabs work

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,27 @@
- 
+let skillTabs = document.querySelectorAll('.tabs li');
+
+
+/* adding an event listener for each tab instead of just on <ul>
+    allows for prevention of clicking the gap inbetween the tabs and
+    triggering a removal of .selected styling.
+*/
+
+skillTabs.forEach(tab => {
+    tab.addEventListener('click', function(e) {
+        let tabsSelected = document.querySelectorAll('.selected')
+
+        tabsSelected.forEach(el => {
+            el.classList.remove('selected')
+        });
+        e.target.classList.add('selected')
+
+        let skillSets = document.querySelectorAll(".skills ul")
+        skillSets.forEach(set => {
+            set.classList.add("hide")
+            if (e.target.dataset.type == set.classList[0]) {
+                set.classList.remove("hide")
+            }
+        })
+
+    })
+})
